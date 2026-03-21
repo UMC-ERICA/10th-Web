8a182f4c4f2a283209611f056a673156c9b629db
seooyoon <bsyoni_8683@naver.com>
2026-03-21 12:06:05 +0900

    버튼 수정

--- 루미-배서윤/wk1/src/wk1.ts
+++ 루미-배서윤/wk1/src/wk1.ts
@@ -5,31 +5,34 @@ const doneList = document.getElementById("done-list") as HTMLUListElement;
 
 addBtn.addEventListener("click", () => {
   const text = input.value.trim();
-
-  if (text === "") return;
+  if (!text) return;
 
   const li = document.createElement("li");
-  li.textContent = text;
+
+  const span = document.createElement("span");
+  span.innerText = text;
 
   const completeBtn = document.createElement("button");
-  completeBtn.textContent = "완료";
+  completeBtn.innerText = "완료";
   completeBtn.className = "complete-btn";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "삭제";
-  deleteBtn.className = "delete-btn";
 
   completeBtn.addEventListener("click", () => {
     doneList.appendChild(li);
     completeBtn.remove();
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "삭제";
+    deleteBtn.className = "delete-btn";
+
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+    });
+
+    li.appendChild(deleteBtn);
   });
 
-  deleteBtn.addEventListener("click", () => {
-    li.remove();
-  });
-
+  li.appendChild(span);
   li.appendChild(completeBtn);
-  li.appendChild(deleteBtn);
 
   todoList.appendChild(li);
 
