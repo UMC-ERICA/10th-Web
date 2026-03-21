a3809d8d17f17840856878a04ad2835098fa19ca
jibeom choi <jb040222@hanyang.ac.kr>
2026-03-21 23:02:58 +0900

    1주차 미션

---
+++ 제비-최지범/Chapter 01 - TypeScript의 기본/src/typeToDo.ts
@@ -0,0 +1,43 @@
+const todoInput = document.getElementById("todoInput") as HTMLInputElement;
+const add_button = document.getElementById("add_button");
+const todoList = document.getElementById("todoList");
+const doneList = document.getElementById("doneList");
+
+const handleAddToDo = (text: string) => {
+  //미리 스타일 된 투두 생성
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.textContent = text;
+  todo.appendChild(content);
+
+  const doneButton = document.createElement("button");
+  doneButton.classList.add("doneButton");
+  doneButton.textContent = "완료";
+  todo.appendChild(doneButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.textContent = "삭제";
+  todo.appendChild(deleteButton);
+
+  //리스트에 추가
+  todoList?.appendChild(todo);
+
+  //이벤트 리스너 등록
+  doneButton.addEventListener("click", () => {
+    doneList?.appendChild(todo);
+  });
+  deleteButton.addEventListener("click", () => {
+    todo.remove();
+  });
+};
+
+add_button?.addEventListener("click", () => {
+  const text = todoInput?.value.trim() ?? "";
+  if (!text) return;
+  handleAddToDo(text);
+  if (todoInput) todoInput.value = "";
+});
